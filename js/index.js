48fd8f0019e696d196e3b92f84bcd2078e18b86c
linaresniper <linares.salguero2027@hilasal.superate.org>
2026-04-12 15:15:37 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function totalPrice() {
+    let purchaseAmount = Number(document.getElementById("purchaseAmount").value);
+    let hasCoupon = document.getElementById("hasCoupon").value === "true";
+
+    if (hasCoupon == true) {
+        let total = purchaseAmount - (purchaseAmount * 0.30);
+        document.getElementById("totalPrice").textContent =
+        "premium discount applied (30%). total price: $" + final;
+
+    } else if (purchaseAmount > 100) {
+        let total = purchaseAmount - (purchaseAmount * 0.20);
+        document.getElementById("totalPrice").textContent =
+        "your discount is 20%. total price: $" + final;
+
+    } else if (purchaseAmount > 50) {
+        let total = purchaseAmount - (purchaseAmount * 0.10);
+        document.getElementById("totalPrice").textContent =
+        "your discount is 10%. total price: $" + final;
+
+    } else {
+        document.getElementById("totalPrice").textContent =
+        "no discount. total price: $" + purchaseAmount;
+    }
+}
